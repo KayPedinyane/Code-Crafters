95f828802c646a6db817e97aa609fd0ca4e1f6d4
KayPedinyane <Kaypedinyane@gmail.com>
2026-04-02 23:32:44 +0200

    Initial project setup

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+// Simple Express server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
