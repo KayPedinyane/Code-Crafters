9e8d8f57bb7baee56ffb7b550c35f59de937ede8
KayPedinyane <Kaypedinyane@gmail.com>
2026-04-04 23:34:18 +0200

    added fetch to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    fetch('https://crafters-backend-fbbrctfrdsegcqdj.southafricanorth-01.azurewebsites.net/')
+      .then(res => res.text())
+      .then(data => setStatus(data))
+      .catch(err => setStatus('Backend not reachable'));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
