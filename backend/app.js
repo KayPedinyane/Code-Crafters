84bcee83670390654c4b1e47aee33c6aa8dca5df
KayPedinyane <Kaypedinyane@gmail.com>
2026-04-04 23:37:35 +0200

    Connect frontend to backend API

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+  origin: 'https://code-crafters-beige.vercel.app'
+}));
 
 app.get('/', (req, res) => {
     res.send("API running");
