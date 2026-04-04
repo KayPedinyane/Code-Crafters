0f910d31cfbac937c7f351f526b3167eb6ba40d5
KayPedinyane <Kaypedinyane@gmail.com>
2026-04-05 01:20:47 +0200

    Allow all CORS origins for testing

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
-  origin: 'https://code-crafters-beige.vercel.app'
-}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("API running");
