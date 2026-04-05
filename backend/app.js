97367eca0ffd50cf4a8f257645b0516a465ab5c3
KayPedinyane <Kaypedinyane@gmail.com>
2026-04-05 13:53:54 +0200

    Switched backend to Render

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://code-crafters-beige.vercel.app'
+}));
 
 app.get('/', (req, res) => {
     res.send("API running");
